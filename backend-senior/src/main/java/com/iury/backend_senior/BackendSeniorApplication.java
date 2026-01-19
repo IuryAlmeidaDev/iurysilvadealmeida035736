6d0ad53d4b2f10d9385cd753c630c60221360f7a
IuryAlmeidaDev <iury.colombo@gmail.com>
2026-01-19 19:18:35 -0400

    Commit inicial do projeto

---
+++ backend-senior/src/main/java/com/iury/backend_senior/BackendSeniorApplication.java
@@ -0,0 +1,13 @@
+package com.iury.backend_senior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSeniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSeniorApplication.class, args);
+	}
+
+}
---
+++ backend-senior/src/test/java/com/iury/backend_senior/BackendSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iury.backend_senior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
