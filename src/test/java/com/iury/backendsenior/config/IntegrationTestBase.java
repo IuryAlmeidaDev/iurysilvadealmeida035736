89aa0d30f30150699a555f17dd1dfbd0a405a4c5
IuryAlmeidaDev <iury.colombo@gmail.com>
2026-01-28 00:55:04 -0400

    test(config): define IntegrationTestBase para suporte ao banco H2

---
+++ src/test/java/com/iury/backendsenior/config/IntegrationTestBase.java
@@ -0,0 +1,11 @@
+package com.iury.backendsenior.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Transactional
+public abstract class IntegrationTestBase {
+}
